4e68b80b558627158a9603d1cb0d50f867b6c376
Raamon Paiva <raamonpaiva@gmail.com>
2023-07-05 15:55:18 -0400

    Created NewTransaction Dialog

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,30 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 
+import * as Dialog from '@radix-ui/react-dialog';
+
 import logoImg from '../../assets/logo.svg';
 
 export function Header() {
   return (
     <HeaderContainer>
       <HeaderContent>
-
         <img src={logoImg} alt="" />
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova Transação</NewTransactionButton>
+          </Dialog.Trigger>
 
-        <NewTransactionButton>Nova Transação</NewTransactionButton>
+          {/** Pop-up window fora de todas as divs, como se fosse algo a parte da aplicação */}
+          <Dialog.Portal>
+            {/** Deixa o fundo com uma opacidade menor e da focus na pop-up */}
+            <Dialog.Overlay />
+
+            <Dialog.Content>
+              <Dialog.Title>Nova Transação</Dialog.Title>
+              <Dialog.Close />
+            </Dialog.Content>
+          </Dialog.Portal>
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   )
