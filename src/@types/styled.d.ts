e729ebbae6396780404e9a3dbd921fc7632cd3ba
Raamon Paiva <raamonpaiva@gmail.com>
2023-07-05 09:40:56 -0400

    Added Logo.svg

--- src/@types/styled.d.ts
+++ src/@types/styled.d.ts
@@ -5,5 +5,5 @@ type ThemeType = typeof defaultTheme
 
 declare module 'styled-components' {
   // eslint-disable-next-line @typescript-eslint/no-empty-interface
-  export interface DefaultTheme extends ThemeType {}
+  export interface DefaultTheme extends ThemeType { }
 }
