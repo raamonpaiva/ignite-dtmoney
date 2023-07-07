c52b643753a59f9519f99ca9cb2b39ef908e04e6
Raamon Paiva <raamonpaiva@gmail.com>
2023-07-07 09:59:50 -0400

    Added UseMemo

--- src/hooks/useSummary.ts
+++ src/hooks/useSummary.ts
@@ -1,5 +1,6 @@
 /** Criação de um hook abstrai a parte logica de um componte, deixando o código mais limpo e de facil manutenção */
 
+import { useMemo } from 'react'
 import { useContextSelector } from 'use-context-selector'
 import { TransactionContext } from '../contexts/TransactionsContext'
 
@@ -9,24 +10,26 @@ export function useSummary() {
   })
 
   /** Converte transactions para um objeto, onde o ultimo parametro é o formato desejado para apresentação dos dados */
-  const summary = transactions.reduce(
-    (acumulator, transaction) => {
-      /** A cada iteração ele faz o somatorio to total dependendo da operação escolhida */
-      if (transaction.type === 'income') {
-        acumulator.income += transaction.price
-        acumulator.total += transaction.price
-      } else {
-        acumulator.outcome += transaction.price
-        acumulator.total -= transaction.price
-      }
+  const summary = useMemo(() => {
+    return transactions.reduce(
+      (acumulator, transaction) => {
+        /** A cada iteração ele faz o somatorio to total dependendo da operação escolhida */
+        if (transaction.type === 'income') {
+          acumulator.income += transaction.price
+          acumulator.total += transaction.price
+        } else {
+          acumulator.outcome += transaction.price
+          acumulator.total -= transaction.price
+        }
 
-      return acumulator
-    },
-    {
-      income: 0,
-      outcome: 0,
-      total: 0,
-    },
-  )
+        return acumulator
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    )
+  }, [transactions])
   return summary
 }
